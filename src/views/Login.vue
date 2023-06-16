7a933435247435fb761bc67c88274f0faf1195dd
Wesley Leal <wesley7leal@outlook.com>
2023-06-16 10:20:12 -0300

    commit que finaliza a view de login, verificações funcionando

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,28 +1,37 @@
 <script>
-  export default {
-    data: () => ({
-      rules: [
-        value => !!value || 'Required.',
-        value => (value && value.length >= 3) || 'Min 3 characters',
-      ],
-    }),
+export default {
+  data: () => ({
+    isFormValid: true,
+    email: "",
+    password: "",
+    emailRules: [(v) => /.+@.+\..+/.test(v) || "E-mail inválido"],
+    rulesPass: [
+      (value) => (value && value.length >= 8) || "Min 8 characters",
+      (v) =>
+        /^(?=.*\d)(?=.*[!@#$%^&*])/.test(v) ||
+        "A senha deve conter pelo menos um número e um caractere especial",
+    ],
+  }),
   }
 </script>
 
 <template>
+  <v-form @submit.prevent v-model="isFormValid">
+    <div>
+      <v-text-field
+        v-model="email"
+        :rules="emailRules"
+        label="E-mail"
+      ></v-text-field>
 
+      <v-text-field
+        v-model="password"
+        label="Senha"
+        :rules="rulesPass"
+        required
+      ></v-text-field>
+    </div>
+  </v-form>
 
-  <div>
-    <v-text-field
-      label="Insira aqui seu e-mail@exemple.com"
-      :rules="rules"
-      hide-details="auto"
-    ></v-text-field>
-    <v-text-field label="Senha"></v-text-field>
-  </div>
-
-  <v-btn>
-  Logar
-</v-btn>
-
+  <v-btn :disabled="!isFormValid"> Logar </v-btn>
 </template>
