5dcf64fc9a91b60ab3cdb618aa27d1e2bbf199ee
Wesley Leal <wesley7leal@outlook.com>
2023-06-19 11:45:11 -0300

    commit para finalizar o input de senha

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -18,15 +18,14 @@ export default {
     rulesPass: [
       (value) => (value && value.length >= 8) || "Min 8 characters",
       (v) =>
-        /^(?=.*\d)(?=.*[! " # $ % & ' ( ) * + , - . / : ; < = > ? @   ^ _` { | } ~ ])/.test(v) ||
-        "A senha deve conter pelo menos um número e um caractere especial",
+        /^(?=.*\d)(?=.*[! " # $ % & ' ( ) * + , - . / : ; < = > ? @   ^ _` { | } ~ ])/.test(
+          v
+        ) || "A senha deve conter pelo menos um número e um caractere especial",
     ],
-    
   }),
   methods: {
     isPasswordMatch(value) {
       return value === this.password || "As senhas não coincidem";
-
     },
   },
 };
@@ -34,53 +33,53 @@ export default {
 
 <template>
   <main class="d-flex flex-column justify-center align-center h-100">
-  <div class="d-flex flex-column justify-center align-center pa-8 w-80% rounded-lg bg-white">
-    <h1 class="text-black">DADOS PARA CADASTRO</h1>
-  <v-form @submit.prevent v-model="isFormValid">
-    <v-text-field
-      v-model="user"
-      label="Nome de Usuário"
-      :rules="userRules"
-      class="input-field "
-    ></v-text-field>
+    <div
+      class="d-flex flex-column justify-center align-center pa-8 w-80% rounded-lg bg-white"
+    >
+      <h1 class="text-black">DADOS PARA CADASTRO</h1>
+      <v-form @submit.prevent v-model="isFormValid">
+        <v-text-field
+          v-model="user"
+          label="Nome de Usuário"
+          :rules="userRules"
+          class="input-field"
+          autofocus
+        ></v-text-field>
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      class="input-field "
-    ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          class="input-field"
+        ></v-text-field>
 
-    <v-text-field
-      v-model="password"
-      label="Senha"
-      :rules="rulesPass"
-      class="input-field "
-      
-    ></v-text-field>
+        <v-text-field
+          v-model="password"
+          label="Senha"
+          :rules="rulesPass"
+          required
+          class="input-field"
+        ></v-text-field>
 
-    <v-text-field
-      v-model="confirmPassword"
-      :rules=[isPasswordMatch]
-      label= "Confirmar Senha"
-      class="input-field"
-      required
-    ></v-text-field>
-  </v-form>
+        <v-text-field
+          v-model="confirmPassword"
+          :rules="[isPasswordMatch]"
+          label="Confirmar Senha"
+          class="input-field"
+          required
+        ></v-text-field>
+      </v-form>
 
-  <v-btn class="me-4" type="submit" :disabled="!isFormValid">
-      Cadastrar
-    </v-btn>
-
-</div>
-</main>
+      <v-btn class="me-4" type="submit" :disabled="!isFormValid">
+        Cadastrar
+      </v-btn>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-
-.input-field
-{
-  background-color: #E7F1D5;
+.input-field {
+  background-color: #e7f1d5;
   width: 600px;
   height: 50px;
   margin: 40px;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,53 +1,57 @@
 <script>
 export default {
   data: () => ({
+    show1: true,
+    show2: false,
     isFormValid: true,
     email: "",
     password: "",
     emailRules: [(v) => /.+@.+\..+/.test(v) || "E-mail inválido"],
-    rulesPass: [
-      (value) => (!!value) || "Senha inválida",
-  ],
+    rulesPass: [(value) => !!value || "Senha inválida"],
   }),
-  }
+  mounted() {
+    this.isFirstFieldFocused = true;
+  },
+};
 </script>
 
 <template>
-  <main class="d-flex flex-column justify-center align-center h-100 ">
+  <main class="d-flex flex-column justify-center align-center h-100">
+    <v-container
+      class="display d-flex flex-column justify-center align-center rounded-xl w-auto bg-white"
+    >
+      <v-form @submit.prevent v-model="isFormValid">
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="E-mail"
+          class="input-field"
+          ref="firstField"
+          autofocus
+        ></v-text-field>
 
-      <v-container class="display d-flex flex-column justify-center align-center rounded-xl w-auto bg-white">
+        <v-text-field
+          :append-icon="show2 ? 'mdi-eye' : 'mdi-eye-off'"
+          :rules="rulesPass"
+          :type="show2 ? 'text' : 'password'"
+          name="input-10-2"
+          label="Senha"
+          class="input-field"
+          @click:append="show2 = !show2"
+        ></v-text-field>
+      </v-form>
 
-  <v-form @submit.prevent v-model="isFormValid">
-      <v-text-field
-        v-model="email"
-        :rules="emailRules"
-        label="E-mail"
-        class="input-field"
-      ></v-text-field>
-
-      <v-text-field
-        v-model="password"
-        label="Senha"
-        :rules="rulesPass"
-        required
-        class="input-field "
-      ></v-text-field>
-  </v-form>
-
-  <v-btn  :disabled="!isFormValid" elevation="8"> Logar </v-btn>
-</v-container>
-
-</main>
-
+      <v-btn :disabled="!isFormValid" elevation="8">Logar</v-btn>
+    </v-container>
+  </main>
 </template>
 
 <style scoped>
-
-.input-field{
-  background-color: #E7F1D5;
+.input-field {
+  background-color: #e7f1d5;
   width: 300px;
   height: 50px;
   margin: 50px;
   color: black;
- }
+}
 </style>
