351eade0c99fb52afba839c48c38d26a7509f083
Rafashhe <cwb.rafaella@gmail.com>
2023-06-23 13:07:35 -0300

    cria chamada de api para criar novas listas

--- src/api/toDoLists.js
+++ src/api/toDoLists.js
@@ -4,6 +4,10 @@ export const toDoListsApiMixin ={
     methods: {
         async list () {
             return await privateApi.get("/lists")
-        }
+        },
+        async createList(payload) {
+            const res = await privateApi.post("/lists/new", payload);
+            return res;
+          },
     }
 }
