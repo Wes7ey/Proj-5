a28758ee4b432133f1035b150ea477dac9bb1a3d
Rafashhe <cwb.rafaella@gmail.com>
2023-06-23 13:09:06 -0300

    cria função para criar novas listas com o chamado da api e insere campo de input com botão no template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <v-card v-for="list in toDoLists" :key="list.id">
-    <v-card-title>{{ list.title }}</v-card-title>
-  </v-card>
+  <div>
+    <v-form class="my-5">
+      <v-text-field v-model="listTitle" placeholder="Nome da lista"></v-text-field>
+      <v-btn @click="createNewList">Criar lista</v-btn>
+    </v-form>
+
+    <v-card v-for="list in toDoLists" :key="list.id">
+      <v-card-title>{{ list.title }}</v-card-title>
+    </v-card>
+
+  </div>
 </template>
 
 <script>
 import { toDoListsApiMixin } from "@/api/toDoLists";
+
 export default {
   mixins: [toDoListsApiMixin],
+
   data() {
     return {
       toDoLists: [],
+      listTitle: "",
     };
   },
+
   methods: {
     async getLists() {
       try {
@@ -22,7 +34,21 @@ export default {
         alert("Algo deu errado");
       }
     },
+
+    async createNewList() {
+      try {
+      const payload = {
+        title: this.listTitle,
+      };
+        await this.createList(payload);
+        this.listTitle = "";
+        this.getLists();
+      } catch (err) {
+        alert("Erro ao criar a lista");
+      }
+    },
   },
+
   mounted() {
     this.getLists();
   },
