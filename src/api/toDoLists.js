6877ee2c81bd47e811bdc40e1a1b6c329e47b5b3
Rafashhe <cwb.rafaella@gmail.com>
2023-06-26 09:27:01 -0300

    define novos caminhos da chamada de APIs para editar e deletar as lists

--- src/api/toDoLists.js
+++ src/api/toDoLists.js
@@ -1,13 +1,22 @@
 import { privateApi } from "./index";
 
-export const toDoListsApiMixin ={
-    methods: {
-        async list () {
-            return await privateApi.get("/lists")
-        },
-        async createList(payload) {
-            const res = await privateApi.post("/lists/new", payload);
-            return res;
-          },
-    }
-}
+export const toDoListsApiMixin = {
+  methods: {
+    async list() {
+      return await privateApi.get("/lists");
+    },
+    async createList(payload) {
+      const res = await privateApi.post("/lists/new", payload);
+      return res;
+    },
+    async updateList(id, payload) {
+      const res = await privateApi.patch(`/lists/${id}`, payload);
+      return res;
+    },
+
+    async deleteList(id) {
+      const res = await privateApi.delete(`/lists/${id}`);
+      return res;
+    },
+  },
+};
